3a4b39f6f5232754cb4e6ae94e64882bd74c5012
Martin Oberzalek <kingleo@gmx.at>
2023-03-03 23:28:01 +0100

    Added script to crosscompile in a cygwin environment
    where cmake is the one shipped with cygwin

---
+++ build_cygwin_mingw_qt.sh
@@ -0,0 +1,15 @@
+#/usr/bin/bash
+
+QTDIR=/cygdrive/c/Qt/6.4.2/mingw_64
+
+cmake  -DCMAKE_TOOLCHAIN_FILE=TC-mingw.cmake .
+for JSON in `find CMakeFiles -name "*.json"` ; do
+	sed -si  "s|/cygdrive.*/moc.exe|`pwd`/mymoc.sh|" ${JSON}
+done
+sed -s "s|QTDIR|${QTDIR}|" mymoc.sh_ > mymoc.sh && chmod a+x mymoc.sh
+
+make -j4
+mkdir dist
+cp -u supagrep.exe dist
+cp -u ${QTDIR}/bin/*.dll dist
+mkdir -p dist/plugins/platforms/ && cp -a ${QTDIR}/plugins/platforms/*.dll dist/plugins/platforms/
