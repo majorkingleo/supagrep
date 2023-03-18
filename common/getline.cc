f67fd615d26547d01e44b52c49586adf0d379c3b
Martin Oberzalek <kingleo@gmx.at>
2023-03-18 22:00:04 +0100

    Added getline from replace

---
+++ common/getline.cc
@@ -0,0 +1,77 @@
+#include "getline.h"
+#include <vector>
+#include <string_utils.h>
+
+using namespace Tools;
+
+std::wstring getline( const std::wstring & s, std::wstring::size_type pos )
+{
+  std::wstring::size_type end = s.find( L'\n', pos );
+
+  return s.substr( pos, end - pos );
+}
+
+
+std::wstring::size_type get_pos_for_line( const std::wstring & content, int line )
+{
+	std::wstring::size_type pos = 0;
+
+	for( int count = 1; count < line; count++ )
+	{
+		pos = content.find(L'\n', pos );
+
+		if( pos == std::string::npos )
+			return pos;
+
+		pos++;
+	}
+
+	return pos;
+}
+
+
+std::string diff_lines( const std::string & orig, std::string & modded )
+{
+  std::vector<std::string> sl_orig, sl_modded;
+
+  sl_orig = split_simple( orig, "\n" );
+  sl_modded = split_simple( modded, "\n" );
+
+  std::string res;
+
+  for( unsigned i = 0; i < sl_orig.size() && i < sl_modded.size(); i++ )
+	{
+	  if( sl_orig[i] != sl_modded[i] )
+		{
+		  if( !res.empty() )
+			res += '\n';
+
+		  res += "\t" + strip( sl_orig[i] ) + " => " + strip( sl_modded[i] );
+		}
+	}
+
+  return res;
+}
+
+std::wstring diff_lines( const std::wstring & orig, std::wstring & modded )
+{
+  std::vector<std::wstring> sl_orig, sl_modded;
+
+  sl_orig = split_simple( orig, L"\n" );
+  sl_modded = split_simple( modded, L"\n" );
+
+  std::wstring res;
+
+  for( unsigned i = 0; i < sl_orig.size() && i < sl_modded.size(); i++ )
+	{
+	  if( sl_orig[i] != sl_modded[i] )
+		{
+		  if( !res.empty() )
+			res += '\n';
+
+		  res += L"\t" + strip( sl_orig[i] ) + L" => " + strip( sl_modded[i] );
+		}
+	}
+
+  return res;
+}
---
+++ common/getline.h
@@ -0,0 +1,13 @@
+#ifndef GETLINE_H
+#define GETLINE_H
+
+#include <string>
+
+std::wstring getline( const std::wstring & s, std::wstring::size_type pos );
+
+std::wstring::size_type get_pos_for_line( const std::wstring & content, int line );
+
+std::string diff_lines( const std::string & orig, std::string & modded );
+std::wstring diff_lines( const std::wstring & orig, std::wstring & modded );
+
+#endif
