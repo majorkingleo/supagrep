4724cd3f6e3600175c61c75065220b690c1e70f5
Martin Oberzalek <kingleo@gmx.at>
2023-06-09 19:29:56 +0200

    Build with debug flags

--- build_cygwin_mingw_qt.sh
+++ build_cygwin_mingw_qt.sh
@@ -3,12 +3,13 @@
 QTDIR=/cygdrive/c/Qt/6.4.3/mingw_64
 
 cmake  -DCMAKE_TOOLCHAIN_FILE=TC-mingw.cmake .
+#cmake  -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=TC-mingw.cmake .
 for JSON in `find CMakeFiles -name "*.json"` ; do
 	sed -si  "s|/cygdrive.*/moc.exe|`pwd`/mymoc.sh|" ${JSON}
 done
 sed -s "s|QTDIR|${QTDIR}|" mymoc.sh_ > mymoc.sh && chmod a+x mymoc.sh
 
-make -j4
+make -j8
 mkdir dist
 cp -u supagrep.exe dist
 cp -u ${QTDIR}/bin/*.dll dist
